2c864aa07827bc9019acdea5547e1ac4583932a1
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-23 15:32:28 +0200

    prologue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image
 import streamlit as st
 from multiapp import MultiApp
 
-from apps import home, about, data, graph, graph2# import your app modules here
+from apps import home, about, data, graph, graph2, result# import your app modules here
 
 app = MultiApp()
 
@@ -11,7 +11,7 @@ app = MultiApp()
 st.markdown("![title gif](https://media4.giphy.com/media/gXYlKennAlNfy/200w.webp?cid=ecf05e475f1zz2nhrj1xonfu9m5j6toky5akxiqi2ylxps84&rid=200w.webp&ct=g)")
 
 
-st.markdown(' ## Team General Grievous welcomes you to our website')
+st.markdown(''' ## Team General Grievous welcomes you to our website''')
 st.text(' ')
 st.markdown('----------------------------------------------- ')
 st.text(' ')
@@ -29,6 +29,7 @@ app.add_app("About", about.app)
 app.add_app("Data", data.app)
 app.add_app('Galaxies Analysis', graph.app)
 app.add_app('Planets Analysis', graph2.app)
+app.add_app('Prologue', result.app)
 
 
 
--- apps/result.py
+++ apps/result.py
@@ -1,6 +1,23 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+def app():
+    
+    text = st.beta_container()
+    image = st.beta_container()
+   
+    
+    
+    with text:
+        st.markdown(''' 
+        ### We thank you for your time and patience.
+        ### We hope that you will have a nice day and... 
+        ''')
+        st.text(' ')
+        st.text(' ')
+        st.markdown('----------------------------------------------- ')
+    
+    with image:
+        image = Image.open('the_force.jpeg')
+        st.image(image, caption='')
